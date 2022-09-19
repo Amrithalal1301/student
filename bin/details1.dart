fd36f3fba64785f9cc690e2bea82543ed5487c76
AMRITHA LAL <lalamritha42@gmail.com>
2022-09-19 13:41:33 +0530

    Initial commit

---
+++ bin/details1.dart
@@ -0,0 +1,10 @@
+void main(){
+
+
+  String name = "Amritha";
+  int age =22;
+  String mailid = "lalamritha42@gmail.com";
+  int num = 8281335384;
+  String adr ="Amrutham,Kadappakada ,Kollam";
+  print (" My name is ${name} \n I am ${age} yrs old \n email -- ${mailid} \n phone number = ${num} \n Address -- ${adr}");
+}
---
+++ test/details1_test.dart
@@ -0,0 +1,8 @@
+import 'package:details1/details1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
